f3376b86c02e02e8a732bbec47ccc089995853e4
winfilder <winfildertchaya@gmail.com>
2023-02-27 00:17:24 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
  * main - A program that prints a line with puts function
- * Return:0(success)
+ * Return: 0 (success)
  */
 int main(void)
 {
---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - A program that prints the size of various computer types
+ * Return 0 
+ */
+int main(void)
+{
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float f;
+printf("Size of a char: %1u byte(s)\n", (unsigned long)sizeof(a));
+printf("size of an int: %1u byte(s)\n",(unsigned long)sizeof(b));
+printf("size of an int: %1u byte(s)\n",(unsigned long)sizeof(c));
+printf("size of an int: %1u byte(s)\n",(unsigned long long)sizeof(d));
+printf("size of a float: %1u byte(s)\n",(unsigned long)sizeof(f));
+return (0);
+}
