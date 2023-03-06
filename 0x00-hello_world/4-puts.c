f64f66c3865b68789c0346f0859f3f88c956932c
winfilder <winfildertchaya@gmail.com>
2023-03-05 16:30:16 -0800

    my first of c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
-/*
- * main - A program that prints a line with puts function
+/**
+ * main - print a line of code using puts
  * Return: 0 (success)
  */
 int main(void)
 {
-puts("\"Programming is like building a miltilingual puzzle"):
-return (0):
+puts("\"Programming is like building a miltilingual puzzle");
+return (0);
 }
