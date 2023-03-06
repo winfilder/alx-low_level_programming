5affe6e1dc15ee1e137a52589d84856077a9b861
winfilder <winfildertchaya@gmail.com>
2023-03-05 16:53:24 -0800

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,11 @@
-#include <stdio.n>
+#include <stdio.h>
 /**
- * main - A program that uses the printf  function
+ * main - print a c program using printf
+ *
  * Return: 0 (success)
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n")
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 		return (0);
 }
