2acf3ba1ba8ee0500d204e8033c1ca91f743f02c
winfilder <winfildertchaya@gmail.com>
2023-03-05 17:44:32 -0800

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,8 +13,8 @@ int main(void)
 	float f;
 printf("Size of a char: %1u byte(s)\n", (unsigned long)sizeof(a));
 printf("size of an int: %1u byte(s)\n", (unsigned long)sizeof(b));
-printf("size of an int: %1u byte(s)\n", (unsigned long)sizeof(c));
-printf("size of an int: %1u byte(s)\n", (unsigned long long)sizeof(d));
+printf("size of a long int: %1u byte(s)\n", (unsigned long)sizeof(c));
+printf("size of a long long int: %1u byte(s)\n", (unsigned long long)sizeof(d);
 printf("size of a float:%1u byte(s)\n", (unsigned long)sizeof(f));
 	return (0);
 }
