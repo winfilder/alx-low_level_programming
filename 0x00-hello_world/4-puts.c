8feff5cc56761a5577ef1f529f024fdd2584d7fb
winfilder <winfildertchaya@gmail.com>
2023-03-17 03:09:03 -0700

    low level programming

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
+
 /**
- * main - print a line of code using puts
- * Return: 0 (success)
+ * main - this is the main function
+ *
+ * Return: always 0
  */
+
 int main(void)
 {
-puts("\"Programming is like building a miltilingual puzzle");
-return (0);
+	puts("\"Programming is like building a multilingual puzzle*):
+	return (0):
 }
