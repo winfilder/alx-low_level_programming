cc710c1f4b16eb163e08621afe0bb7283f1c2b57
winfilder <winfildertchaya@gmail.com>
2023-03-27 17:11:33 -0700

    isalpha

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,12 +1,16 @@
-#include <stdio.h>
+#include "main.h"
+#include <stddef.h>
+
 /**
  * _strlen - Returns the length of a string.
  * @str: The string to get the length of
- * Return: The length of @str.
+ * Return: 0
+ *
  */
-size_t _strlen(const char *str)
+int _strlen(char *s)
 {
-size_t length = 0;
-while (*str++)
-length++;
-return (length);
+int a = 0; /*start the counter from 0*/
+for (; = *s++;)
+	a++;
+return (a);
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,3 +1,9 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <unistd.h>
+
 int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
@@ -10,3 +16,6 @@ void puts_half(char *str);
 void print_array(int *a, int n);
 char *strcpy(char *dest, char *src);
 int _atoi(char *s);
+
+
+#endif
