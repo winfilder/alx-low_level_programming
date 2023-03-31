6f1ab0b58e43314933295b105639d9506c3f48c5
winfilder <winfildertchaya@gmail.com>
2023-03-30 17:21:56 -0700

    function that encodes a string

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
-char *string_toupper(char *);
+char *string_toupper(char *)
 {
 	int i;
 
-	for (i = 0  str[i] != '\0' ; i++)
+	for (i = 0  str[i] != '\0' ; i++);
 	{
 		if (str[i] >= 'a' && str[i] <= 'z')
 			str[i] = str[i] = 32;
